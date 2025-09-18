73313923cfa6b26fe1dec8af4e4d839fc2d57f3e
Prajwal T <73645971+PrajwalGitCode@users.noreply.github.com>
2025-09-18 22:06:04 +0530

    fix

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(cors({
   origin: [
-    "https://stunning-bubblegum-e2eb39.netlify.app",
+    "https://stunning-bubblegum-e2eb39.netlify.app/api",
   ],
   credentials: true,
 }));
